9beeba59990d89ead2095159f908843d8d51b65c
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-18 13:57:44 +0100

    comments

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+/* Create a 2 dimensional array and populate it with random number 0 or 1 */
 function make2DArray(cols, rows) {
-    var arr = new Array(cols);
-    for (let i = 0; i < cols; i++) {
-        arr[i] = new Array(rows);
-        for (j = 0; j < rows; j++) {
-            arr[i][j] = getRandomInt(2);
+    var arr = new Array(cols);          // make an array cols length
+    for (let i = 0; i < cols; i++) {    // for every cols index
+        arr[i] = new Array(rows);       // make a new array of rows length
+        for (j = 0; j < rows; j++) {    // for every rows index
+            arr[i][j] = getRandomInt(2);// generate a random number from 0-1 and insert it
           }
     }
     return arr;
