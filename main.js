af2187868267807440260bdda3687c3ad7b6d046
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-20 20:19:42 +0100

    changed log statements a bit

--- main.js
+++ main.js
@@ -17,13 +17,13 @@
 // Variables in global scope
 var grid;
 var ctx; 
-var resolution = 10;    
+var resolution = 20;    
 var width = 10;
 var height = 10; 
 
 /* Create a 2 dimensional array and populate it with random number 0 or 1 */
 function make2DArray(cols, rows) {
-    console.log("make2DArray called");
+    console.log("creating array");
     var arr = new Array(cols);          // make an array cols length
     for (let i = 0; i < cols; i++) {    // for every cols index
         arr[i] = new Array(rows);       // make a new array of rows length
@@ -32,15 +32,16 @@ function make2DArray(cols, rows) {
 }
 
 function setup() {
-    console.log("setup called");
+    console.log("setup started");
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     grid = make2DArray(resolution, resolution);
     populateArray(grid);
+    console.log("setup exiting");
 }
 
 function populateArray(arr) {
-    console.log("populateArray called");     
+    console.log("populating array");     
     for (let i = 0; i < resolution; i++) {
         for (j = 0; j < resolution; j++) {
             arr[i][j] = Math.floor(Math.random() * Math.floor(2));    // generate a random number from 0-1 and insert it to given index
@@ -50,7 +51,6 @@ function populateArray(arr) {
 }
 
 function render() {
-    console.log("Render called");
     drawGrid(0,0);
     console.log("Render done");
     // Create a new grid by running current grid through logic/gamerules
@@ -62,14 +62,13 @@ function render() {
 
 
 function drawGrid(x, y){
-    console.log("drawColumn called");
+    console.log("rendering grid");
     for (let c = 0; c < resolution; c++){
         for (let r = 0; r < resolution; r++){
             grid[c][r] ? ctx.fillStyle = "#f5f5f5" : ctx.fillStyle = "#000000";
             ctx.fillRect(c*width, r*height, width, height);
         }
     }
-    console.log("drawGrid Done!");
 }
 
 // Runtime
