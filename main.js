09471252396f74b79560db7575e50817d965e85d
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-20 10:49:04 +0100

    rewrote drawSquare to drawColumn. drawColumn is now called rows.length times from render() and render increments the x value

--- main.js
+++ main.js
@@ -4,17 +4,13 @@ var cols = 10;
 var rows = 10;
 var resolution = 10;
 var ctx;
-var point;  
-var enpoint;         
-var width = resolution;
-var height = resolution;
+var point = {x: 0, y: 0};        
 
 /* Create a 2 dimensional array and populate it with random number 0 or 1 */
 function make2DArray(cols, rows) {
     var arr = new Array(cols);          // make an array cols length
     for (let i = 0; i < cols; i++) {    // for every cols index
         arr[i] = new Array(rows);       // make a new array of rows length
-
     }
     return arr;
 }
@@ -30,7 +26,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function populateArray(arr) {          
+function populateArray(arr) {
     for (let i = 0; i < cols; i++) {        // for every cols index 
         for (j = 0; j < rows; j++) {        // for every rows index
             arr[i][j] = getRandomInt(2);    // generate a random number from 0-1 and insert it to given index
@@ -41,37 +37,23 @@ function populateArray(arr) {
 
 function render() {
 
+
     for (let i = 0; i<cols; i++){
-        for(let j = 0; j<rows; j++){
-
-            if (!point){
-                point = {
-                    x: 0,
-                    y: 0,
-                };
-            }
-
-            drawSquare(point);
-
-
-        }
+        drawColumn(point.x, point.y, cols);
+        point.x += resolution;
     }
+    
 }
 
-// helper function for render
-function drawSquare({x, y}) {
-    console.log("printing a square, oops i mean draw!");
-    var endPoint = {
-        x: x + width,
-        y: y + height,
-    };
-    ctx.beginPath();
-    ctx.rect(point.x, point.y, endpoint.x, endpoint.y);
-    ctx.fillStyle = "#000000";
-    ctx.fill();
-    ctx.closePath();
-    
-    return endPoint;
+
+function drawColumn(x, y, cols){
+    for (i = 1; i <= cols; i++){
+        ctx.beginPath();
+        ctx.rect(point.x, point.y, x + resolution, y + (resolution * i));
+        ctx.fillStyle = "#000000";
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
 // Runtime
